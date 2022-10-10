66aee7c7e3e0844aeae9ef911eb2642f61afa712
Packy <lpreterite@126.com>
2022-10-10 15:37:22 +0800

    support media file base on md file path

--- src/main.ts
+++ src/main.ts
@@ -27,9 +27,13 @@ export default class LocalImagesPlugin extends Plugin {
 
   private async proccessPage(file: TFile, silent = false) {
     // const content = await this.app.vault.read(file);
+    let mediaRootDirectory = this.settings.mediaRootDirectory
+    if(this.settings.mediaRootDirectoryBaseOnFile){
+      mediaRootDirectory = file.parent.path + "/" + this.settings.mediaRootDirectory.replace(/^(\/|\.\/)/,"")
+    }
     const content = await this.app.vault.cachedRead(file);
 
-    await this.ensureFolderExists(this.settings.mediaRootDirectory);
+    await this.ensureFolderExists(mediaRootDirectory);
 
     const cleanedContent = this.settings.cleanContent
       ? cleanContent(content)
@@ -37,7 +41,7 @@ export default class LocalImagesPlugin extends Plugin {
     let fixedContent = await replaceAsync(
       cleanedContent,
       EXTERNAL_MEDIA_LINK_PATTERN,
-      imageTagProcessor(this.app, this.settings.mediaRootDirectory)
+      imageTagProcessor(this.app, mediaRootDirectory)
     );
     
     fixedContent = fixedContent.replace(/\!\[.*\]\((.*)\)/g, "![|]($1)");
@@ -328,6 +332,19 @@ class SettingTab extends PluginSettingTab {
           this.plugin.settings.include = value;
           await this.plugin.saveSettings();
         })
+      );
+
+
+    new Setting(containerEl)
+      .setName("Path is based on the MD file")
+      .setDesc("The path where the media files are saved starts from the MD file directory location!")
+      .addToggle((toggle) =>
+        toggle
+          .setValue(this.plugin.settings.mediaRootDirectoryBaseOnFile)
+          .onChange(async (value) => {
+            this.plugin.settings.mediaRootDirectoryBaseOnFile = value;
+            await this.plugin.saveSettings();
+          })
       );
 
     new Setting(containerEl)
